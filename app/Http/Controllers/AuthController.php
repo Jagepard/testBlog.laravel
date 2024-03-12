c7659e1178668db425aa6b0fd54e28b03aeb76d9
Jagepard <dankorot@gmail.com>
2024-03-12 11:07:23 +0300

    add simple auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Http\Controllers;
+
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email'    => 'required',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return redirect()->intended('admin')->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    public function registration()
+    {
+        return view('auth.register');
+    }
+
+    public function postRegistration(Request $request)
+    {
+        $request->validate([
+            'name'     => 'required',
+            'email'    => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $this->create($request->all());
+
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+    public function create(array $data)
+    {
+        return User::create([
+            'name'     => $data['name'],
+            'email'    => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+    }
+    public function dashboard()
+    {
+        if (Auth::check()) {
+            return view('auth.dashboard');
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return redirect('login');
+    }
+}
